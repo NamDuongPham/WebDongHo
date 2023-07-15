26f80b8b118bf1d377d6165df11645c0afff175f
Nam Duong <phamnamduong583@gmail.com>
2023-07-15 15:00:27 +0700

    frist commit

---
+++ LuxuryWatches/Data/taiKhoanBLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuxuryWatches.Data
+{
+    public class taiKhoanBLL
+    {
+        LuxuryWatchesEntities db = new LuxuryWatchesEntities();
+        public taiKhoan AdminLogin(string tkThanhVien, string matkhau)
+        {
+            return db.taiKhoans.FirstOrDefault(m => m.Quyen == "admin" && m.tkThanhVien == tkThanhVien && m.matKhau == matkhau);
+        }
+        public taiKhoan MemberLogin(string tkThanhVien, string matkhau)
+        {
+            return db.taiKhoans.FirstOrDefault(m => m.Quyen == "member" && m.tkThanhVien == tkThanhVien && m.matKhau == matkhau);
+        }
+    }
+}
---
+++ LuxuryWatches/U_Controls/BottomHeader.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LuxuryWatches.U_Controls
+{
+    public partial class BottomHeader : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtTuKhoa.Attributes.Add("placeholder", "Nhập tên sản phẩm cần tìm...");
+        }
+
+        protected void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string tukhoa = txtTuKhoa.Text;
+            Response.RedirectToRoute("timkiem", new { tukhoa = tukhoa });
+        }
+    }
+}
